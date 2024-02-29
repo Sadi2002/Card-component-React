93122b13c15da58082db9e903f3a00f1f9bfec92
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2024-02-29 22:01:09 +0100

    finis project

--- src/App.js
+++ src/App.js
@@ -1,1 +1,50 @@
-export default function App() {}
+import Logo from "./test.webp";
+import Human from "./human.avif";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Image />
+      <Title />
+      <Description />
+      <Stats />
+      <User />
+    </div>
+  );
+}
+
+function Image() {
+  return <img src={Logo} alt="test" />;
+}
+
+function Title() {
+  return <h1>Camera on sea</h1>;
+}
+function Description() {
+  return (
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates,
+      iusto.
+    </p>
+  );
+}
+
+function Stats() {
+  return (
+    <div className="stats">
+      <span>21.00123</span>
+      <span>1 day left</span>
+    </div>
+  );
+}
+
+function User() {
+  return (
+    <div className="user">
+      <img src={Human} alt="" />
+      <span>
+        Created by <span className="user-name">Gabriel Kolang</span>
+      </span>
+    </div>
+  );
+}
